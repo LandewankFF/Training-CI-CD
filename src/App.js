2541ad4f799607d194ed47912fa038a0e767e793
LandewankFF <landewankfahrezaf@gmail.com>
2024-05-09 09:59:58 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Success CI/CD REACT
         </a>
       </header>
     </div>
