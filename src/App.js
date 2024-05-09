7d6afe585ddeb9865282c7910502c0dcf527fcfb
LandewankFF <landewankfahrezaf@gmail.com>
2024-05-09 10:02:20 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          makan bakso
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Success CI/CD REACT
+          Learn React
         </a>
       </header>
     </div>
